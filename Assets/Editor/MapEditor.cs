f25e5e55fdf4b6b0ef3de5b6d9eb700a5c7bd370
Lenakeiz <andrea.castegnaro@outlook.com>
2015-10-24 00:39:04 +0100

    Added map prefab

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MapController))]
+public class MapEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		base.OnInspectorGUI();
+
+		MapController map = target as MapController;
+		if(GUILayout.Button ("Build Map"))
+		{
+			map.GenerateMap();
+		}
+
+	}
+}
---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+
+	public Transform tilePrefab;
+	public Vector2 MapSize;
+
+	public void GenerateMap()
+	{
+		string holderName = "GeneratedMap";
+		if(transform.FindChild(holderName)){
+			DestroyImmediate(transform.FindChild(holderName).gameObject);
+		}
+
+		Transform mapHolder = new GameObject(holderName).transform;
+		mapHolder.parent = this.transform;
+
+		for (int x = 0;x  < MapSize.x; x++) {
+			for (int y = 0; y < MapSize.y; y++) {
+				Vector3 tilePosition = new Vector3(- MapSize.x/2 + 0.5f + x, 0, - MapSize.y/2 + 0.5f + y);
+				Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+				newTile.parent = mapHolder;
+			}
+		}
+	}
+
+}
